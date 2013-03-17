9b9ee87a8c99399e43fd5c610584bf22ec77984a
Ondra Heřmánek <ondra.hermanek@gmail.com>
2013-03-17 15:46:25 +0100

    ParallelFileSearch added

---
+++ HomeWorks/ParallelFileSearch/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace ParallelFileSearch
+{
+    partial class ParallelFileSearchForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.lbx_Files = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // lbx_Files
+            // 
+            this.lbx_Files.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lbx_Files.FormattingEnabled = true;
+            this.lbx_Files.Location = new System.Drawing.Point(16, 12);
+            this.lbx_Files.Name = "lbx_Files";
+            this.lbx_Files.Size = new System.Drawing.Size(256, 212);
+            this.lbx_Files.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 237);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "lbl_Stats";
+            // 
+            // timer
+            // 
+            this.timer.Interval = 1000;
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // ParallelFileSearchForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lbx_Files);
+            this.Name = "ParallelFileSearchForm";
+            this.Text = "ParallelFileSearch";
+            this.Load += new System.EventHandler(this.ParallelFileSearchForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lbx_Files;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer;
+    }
+}
+
---
+++ HomeWorks/ParallelFileSearch/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace ParallelFileSearch
+{
+    partial class ParallelFileSearchForm : Form
+    {
+        private ParallelFileSearcher searcher;
+        private SortedSet<string> acceptedFiles = new SortedSet<string>();
+        private bool searching = false;
+
+        public ParallelFileSearchForm(ParallelFileSearcher searcher)
+        {
+            this.searcher = searcher;
+            this.searcher.SearchForm = this;
+
+            InitializeComponent();
+        }
+
+        public void AcceptFile(FileInfo file)
+        {
+            this.acceptedFiles.Add(file.Name);
+        }
+
+        public void OnSearchStarted()
+        {
+            this.searching = true;
+            UpdateStats();
+        }
+
+        public void OnSearchFinished()
+        {
+            this.searching = false;
+            UpdateStats();
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            this.lbx_Files.Items.Clear();
+            this.lbx_Files.Items.AddRange(acceptedFiles.ToArray());
+
+            UpdateStats();
+        }
+
+        private void UpdateStats()
+        {
+            int filesProcessed = this.acceptedFiles.Count;
+
+            this.label1.Text = String.Format(
+                "{0} {1} files containing pattern found.",
+                (searching) ? "Searching in progress, so far" : "Searching finished.",
+                filesProcessed
+            );
+
+        }
+
+        private void ParallelFileSearchForm_Load(object sender, EventArgs e)
+        {
+            // Trigger searching
+            new System.Threading.Thread(()=>this.searcher.Run());
+        }
+    }
+}
