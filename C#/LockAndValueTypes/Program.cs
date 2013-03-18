4e18162c950807ffa1a985b3f56db14f2d13872e
Ondra Heřmánek <ondra.hermanek@gmail.com>
2013-03-18 17:48:51 +0100

    Example solution added

---
+++ C#/LockAndValueTypes/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace LockAndValueTypes {
+	class Program {
+		static void Main(string[] args) {
+			// Boxing problem: following does not work!
+
+			DateTime time = new DateTime(2013, 03, 04);
+
+			ThreadStart locker = () => {
+				// Error: 'System.DateTime' is not a reference type as required by the lock statement
+				// lock (time) {
+				Monitor.Enter(time);
+					Console.WriteLine("Thread {0}: Entered critical section.", Thread.CurrentThread.ManagedThreadId);
+					Thread.Sleep(2000);
+					Console.WriteLine("Thread {0}: Leaving critical section.", Thread.CurrentThread.ManagedThreadId);
+				Monitor.Exit(time);		// Throws an exception as we are trying to unlock a lock not locked before
+										// (we locked a lock associated with a completly different instance above)
+			};
+
+			var t1 = new Thread(locker);
+			t1.Start();
+
+			locker();
+			t1.Join();
+
+			// Boxing the value type once - works, but weird -> try to void it!
+
+			Console.WriteLine();
+
+			object time2 = time;
+			ThreadStart locker2 = () => {
+				Monitor.Enter(time2);
+					Console.WriteLine("Thread {0}: Entered critical section.", Thread.CurrentThread.ManagedThreadId);
+					Thread.Sleep(2000);
+					Console.WriteLine("Thread {0}: Leaving critical section.", Thread.CurrentThread.ManagedThreadId);
+				Monitor.Exit(time2);
+			};
+
+			var t2 = new Thread(locker2);
+			t2.Start();
+
+			locker2();
+			t2.Join();
+		}
+	}
+}
---
+++ C#/ThreadAbortRepeated/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace ThreadAbortRepeated {
+	class Program {
+		static void Main(string[] args) {
+
+			var t = new Thread(() => {
+				try {
+					try {
+						Console.WriteLine("Thread 2: Entering main infinite loop ...");
+						while (true) ;
+					} catch (ThreadAbortException) {
+						Console.WriteLine("Thread 2: ThreadAbortException caught. Entering infinite loop ...");
+						// Uncommenting leads to a deadlock below at second Thread.Abort!
+						// Thread.ResetAbort();
+						while (true) ;
+					}
+				} finally {
+					Console.WriteLine("Thread 2: Outer finally block reached. Entering infinite loop ...");
+					while (true) ;
+				}
+			});
+			Console.WriteLine("Main: Second thread created, it's state: {0}", t.ThreadState);
+			t.Start();
+			Console.WriteLine("Main: Second thread started, it's state: {0}", t.ThreadState);
+
+			Thread.Sleep(1000);
+			Console.WriteLine("Main: 1st: Trying to abort second thread in state {0} ...", t.ThreadState);
+			t.Abort();
+
+			Thread.Sleep(1000);
+			Console.WriteLine("Main: 2nd: Trying to abort second thread in state {0} ...", t.ThreadState);
+			t.Abort();
+
+			Thread.Sleep(1000);
+			Console.WriteLine("Main: 3rd: Trying to abort second thread in state {0} ...", t.ThreadState);
+			t.Abort();
+
+			Thread.Sleep(1000);
+			Console.WriteLine("Main: Second thread state: {0}", t.ThreadState);
+			t.IsBackground = true;		// Force thread termination at process exit
+		}
+	}
+}
