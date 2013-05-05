d8b0d92dee8090601271119ee7d4580c7ed1152a
Ondra Hermanek <ondra.hermanek@gmail.com>
2013-05-05 02:10:08 +0200

    Message parser

---
+++ HomeWorks/ChatClient/Client/Client.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net.Sockets;
+using ChatClient.Core;
+using System.Net;
+using Chat.Client.Messages;
+
+namespace Chat.Client
+{
+    class Client: NetworkCommunicator, IDisposable
+    {
+        TcpClient client = null;
+        NetworkStreamInfo streamInfo = null;
+        
+        public string Name { get; set; }
+
+        public event EventHandler<MessageEventArgs> MessageRecived;
+
+        public bool TryConnect(string hostAddress)
+        {
+            if (Connected)
+                throw new InvalidOperationException("Client is already connected.");
+
+            try
+            {
+                var hostEntry = Dns.GetHostEntry(hostAddress);
+
+                if (hostEntry.AddressList.Length == 0)
+                    return false;
+
+                IPAddress address = hostEntry.AddressList[0];
+                var port = Configuration.Configuration.ServerPort;
+
+                TcpClient client = new TcpClient(address.AddressFamily);
+                client.Connect(address, port);
+                streamInfo = new NetworkStreamInfo(client.GetStream());
+
+                StartReading(streamInfo);
+
+                Connected = true;
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Failed to connect: {0}\n{1}", ex.Message, ex.StackTrace);
+            }
+
+            return false;
+        }
+
+        public void SendMessage(IMessage message)
+        {
+            StartSending(message.ToString(), streamInfo);
+            Console.WriteLine("Client {1} sent: >>{0}<<", message, Name);
+        }
+
+        protected override void OnReadFinished(IMessage message, ReadStateObject readState)
+        {
+            Console.WriteLine("Client {1} received >>{0}<<", message, Name);
+            
+            if (MessageRecived != null)
+                MessageRecived(this, new MessageEventArgs() { Data = readState.Data });
+        }
+
+        protected override void OnReadingFailed(Exception ex, ReadStateObject readState)
+        {
+            if (streamInfo != null && streamInfo.Stream != null)
+                streamInfo.Stream.Close();            
+        }
+
+        protected override void OnSendingFailed(Exception ex, SendStateObject sendState)
+        {
+            if (streamInfo != null && streamInfo.Stream != null)
+                streamInfo.Stream.Close();
+        }
+
+        public void Dispose()
+        {
+            if (streamInfo != null && streamInfo.Stream != null)
+                streamInfo.Stream.Close();
+
+            if (client != null)
+                client.Close();
+        }
+    }
+}
---
+++ HomeWorks/ChatClient/Core/MessageParser.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Chat.Client.Messages;
+
+namespace Chat.Client.Core
+{
+    class MessageParser
+    {
+        protected List<IMessage> emptyMessages = new List<IMessage>()
+        {
+            PingMessage.Empty,
+            PongMessage.Empty,
+            AckMessage.Empty,
+            HelloMessage.Empty,
+            HelloMessage.Empty,            
+            OllehMessage.Empty,
+            OllehMessage.Empty,
+            ChatMessage.Empty,
+            ChatMessage.Empty,
+            ChatMessage.Empty,
+            ErrorMessage.Empty,
+        };
+
+        public virtual IMessage ParseMessage(string data)
+        {
+            foreach (var emptyMessage in emptyMessages)
+            {
+                var message = emptyMessage.Matches(data);
+
+                // If matches
+                if (message != null)
+                    return message;
+            }
+
+            // Not matched - unknown message or bad format 
+            return new ErrorMessage(data);
+        }
+    }
+}
